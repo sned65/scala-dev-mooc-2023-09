8cf1eb5d229e6f4327b1e8707dec14a3a0a74b87
ValentinShilin <valentin.shilin.de@gmail.com>
2023-10-16 20:33:33 +0200

    collection 1

--- src/main/scala/module1/datacollections/DataCollection1/PartialFunction.scala
+++ src/main/scala/module1/datacollections/DataCollection1/PartialFunction.scala
@@ -1,1 +1,16 @@
 package module1.datacollections.DataCollection1
+
+object Collect{
+
+  val parseRange: PartialFunction[Any, Int] ={
+    case x: Int if x>10 => x+1
+  }
+
+  def main(args: Array[String]): Unit ={
+    List(15, 3, "dfsdfg").collect(parseRange).foreach(println)
+
+  }
+
+}
+
+
--- src/main/scala/module1/datacollections/DataCollection2/Monada.scala
+++ src/main/scala/module1/datacollections/DataCollection2/Monada.scala
@@ -1,1 +1,2 @@
 package module1.datacollections.DataCollection1
+
