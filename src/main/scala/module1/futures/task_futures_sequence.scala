6b88ef247571a25f291a5939f5628c9b093497c2
Nikolay Smirnov <nicksm65@gmail.com>
2023-10-30 21:53:26 +0300

    HW04

--- src/main/scala/module1/futures/task_futures_sequence.scala
+++ src/main/scala/module1/futures/task_futures_sequence.scala
@@ -3,6 +3,7 @@ package module1.futures
 import module1.futures.HomeworksUtils.TaskSyntax
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 object task_futures_sequence {
 
@@ -20,7 +21,27 @@ object task_futures_sequence {
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`" ()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+
+    // Вспомогательный class. Чисто для удобства.
+    class Result(good: List[A], bad: List[Throwable]) {
+      def add(x : Try[A]): Result = x match {
+        //case Failure(e) => new Result(good, e +: bad) //more efficient, но потом нужен будет reverse
+        case Failure(e) => new Result(good, bad :+ e)
+        //case Success(v) => new Result(v +: good, bad)
+        case Success(v) => new Result(good :+ v, bad)
+      }
+      
+      def toTuple: (List[A], List[Throwable]) = (good, bad)
+    }
+    object Result {
+      val empty = new Result(List.empty[A], List.empty[Throwable])
+    }
+    
+    val start: Future[Result] = Future.successful(Result.empty)
+    futures.foldLeft(start){ case (r, f) =>
+      f.transform { t => Success(r.map(_.add(t))) }.flatten
+    }.map(_.toTuple)
+  }
 
 }
